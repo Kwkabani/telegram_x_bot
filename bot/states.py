6bc00c8769749dd69e12d8af3d5850819b83b037
kwkabani <kwkabani7300@gmail.com>
2026-06-24 18:39:16 +0300

    fix: range(14)->range(13) after removing AWAITING_OAUTH_PIN state

--- bot/states.py
+++ bot/states.py
@@ -4,14 +4,14 @@ from telegram.ext import ConversationHandler
     MAIN_MENU,                      # 0
     AWAITING_LOGIN,                 # 1
     AWAITING_CREDENTIALS,           # 2 - X username:password login
-    AWAITING_POST_TEXT,             # 4
-    AWAITING_POST_MEDIA,            # 5
-    AWAITING_DELETE_TIME,           # 6
-    AWAITING_CUSTOM_DELETE_TIME,    # 7
-    AWAITING_REPEAT_COUNT,          # 8
-    AWAITING_CONFIRMATION,          # 9
-    AWAITING_SETTINGS_DELETE_TIME,  # 10
-    AWAITING_CUSTOM_SETTINGS_DELETE_TIME,  # 11
-    AWAITING_SETTINGS_REPEAT,       # 12
-    AWAITING_SETTINGS_COOLDOWN,     # 13
-) = range(14)
+    AWAITING_POST_TEXT,             # 3
+    AWAITING_POST_MEDIA,            # 4
+    AWAITING_DELETE_TIME,           # 5
+    AWAITING_CUSTOM_DELETE_TIME,    # 6
+    AWAITING_REPEAT_COUNT,          # 7
+    AWAITING_CONFIRMATION,          # 8
+    AWAITING_SETTINGS_DELETE_TIME,  # 9
+    AWAITING_CUSTOM_SETTINGS_DELETE_TIME,  # 10
+    AWAITING_SETTINGS_REPEAT,       # 11
+    AWAITING_SETTINGS_COOLDOWN,     # 12
+) = range(13)
